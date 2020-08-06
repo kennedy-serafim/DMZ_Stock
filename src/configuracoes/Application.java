11963898d7631deae9d5ea0ae889462929068b42
Kennedy Serafim <serafimkennedy@gmail.com>
2020-08-06 11:43:11 +0200

    Core: Adicionando arquivo de configuraçao do sistema e a classe de carregamento do arquivo de configuração

---
+++ src/configuracoes/Application.java
@@ -0,0 +1,35 @@
+package configuracoes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ *
+ * @author Kennedy Melvin Serafim
+ */
+public class Application {
+
+    /**
+     * Responsavel por pegar os dados do arquivo de configuracao do sistema
+     * (properties)
+     *
+     * @return
+     */
+    public static Properties getProperties() {
+        Properties properties = new Properties();
+
+        try (InputStream input = Thread.currentThread().getClass().getResourceAsStream("/configuracoes/application.properties")) {
+            properties.load(input);
+        } catch (IOException ioe) {
+            System.err.println(Arrays.toString(ioe.getStackTrace()));
+        }
+
+        return properties;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getProperties());
+    }
+}
