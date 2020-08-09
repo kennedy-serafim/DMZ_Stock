fb2f87b88c1e4e2db0cf18d04fb8b07b9db8418f
Kennedy Serafim <serafimkennedy@gmail.com>
2020-08-09 03:47:31 +0200

    Core: Adicionando o model para Entrada e Saida  de Produtos

---
+++ src/com/dmz/stock/model/ProdutoSaida.java
@@ -0,0 +1,71 @@
+package com.dmz.stock.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author seraf
+ */
+public class ProdutoSaida {
+
+    private int quantidade;
+    private Date dataSaida;
+    private String observacao;
+    private Usuario usuario;
+    private int id;
+    private Produto produto;
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(Date dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoSaida{" + "quantidade=" + quantidade + ", dataSaida=" + dataSaida + ", observacao=" + observacao + ", usuario=" + usuario + ", id=" + id + ", produto=" + produto + '}';
+    }
+
+}
